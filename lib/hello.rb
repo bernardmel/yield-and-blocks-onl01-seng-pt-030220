e39ad47a6d03651ee0d45882e347aeb7b5d7b332
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-11 01:21:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
 i = 0
 
   while i < array.length
@@ -6,8 +7,10 @@ i = 0
     i = i + 1
   end
 array
+else
+  puts "Hey! No block was given!"
 end
-
+end
 # call your method here!
 ["Tim", "Tom", "Jim"]
 
